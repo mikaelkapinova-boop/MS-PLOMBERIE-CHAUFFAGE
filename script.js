b53d5f9d845d10af2c0bb8ed6fb6a465f3af0764
mikaelkapinova-boop <mikaelkapinova@gmail.com>
2025-09-12 13:35:01 +0200

    Add files via upload
    Signed-off-by: mikaelkapinova-boop <mikaelkapinova@gmail.com>

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Toggle menu
+function toggleMenu() {
+  const nav = document.getElementById("nav-links");
+  const burger = document.querySelector(".burger");
+  nav.classList.toggle("show");
+  burger.classList.toggle("open");
+}
+
+// Toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+// EmailJS
+(function(){
+  emailjs.init("BswSOWv3nFcCn7lQG");
+})();
+
+const contactForm = document.getElementById("contact-form");
+if(contactForm){
+  contactForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    emailjs.send("service_j5g070o","template_10dackj", {
+      from_name: document.getElementById("name").value,
+      from_email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+    }).then(function(response) {
+        document.getElementById("form-message").innerText = "✅ Message envoyé avec succès !";
+        contactForm.reset();
+    }, function(error) {
+        document.getElementById("form-message").innerText = "❌ Erreur lors de l'envoi, réessayez.";
+    });
+  });
+}
